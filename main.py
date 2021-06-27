fe23cc5cc367a844fea5a54b02275262b95b27f0
Newnoob12 <025.sahil@gmail.com>
2021-06-28 00:02:07 +0530

    first upload

--- main.py
+++ main.py
@@ -15,4 +15,4 @@ def hello(message):
     downloaded_file = bot.download_file(file_info.file_path)
     bot.send_video(message.chat.id,data=downloaded_file,caption="sahil is great")
     bot.delete_message(chat_id=message.chat.id,message_id=message.message_id)
-print("hello world")
+#print("hello world")
